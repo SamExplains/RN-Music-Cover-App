16486997fbee561bb0991213974cb919f5132638
Sampuccino <Sampuccino@gmail.com>
2018-06-10 17:42:03 -0700

    Added reading from remote JSON source for application data to use.

--- MusicCoverApp/src/components/AlbumList.js
+++ MusicCoverApp/src/components/AlbumList.js
@@ -3,12 +3,31 @@ import {View, Text} from 'react-native'
 //import axios from 'axios'
 
 class AlbumList extends Component {
+  state = {
+    data: ''
+  }
+  componentWillMount = () => {
+  fetch('https://rallycoding.herokuapp.com/api/music_albums', {
+  method: 'GET'
+})
+.then((response)=> response.json())
+.then((responseJson) =>{
+  console.log(responseJson);
 
+  this.setState({
+    data: responseJson
+  })
+})
+.catch((error) => {
+  console.error(error);
+});
+}
 
   render(){
     return  (
       <View>
         <Text>Album List!</Text>
+        <Text>{this.state.data.body}</Text>
       </View>
     );
   }
