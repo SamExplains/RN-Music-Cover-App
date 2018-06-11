f4d2f9906b33141d034dd58a491e0d1a934f05da
Sampuccino <Sampuccino@gmail.com>
2018-06-10 23:33:25 -0700

    Some more minor progress with the app.

--- MusicCoverApp/src/components/AlbumDetail.js
+++ MusicCoverApp/src/components/AlbumDetail.js
@@ -4,20 +4,25 @@ import Card from './Card';
 import CardSection from './CardSection'
 
 const AlbumDetail = ( { album } ) => {
-  const {title, artist, thumbnail_image } = album;
-  const {thumbnailStyle, headerContentStyle } = styles;
+  const {title, artist, thumbnail_image, image } = album;
+  const {thumbnailStyle, headerContentStyle, headerTextStyle, thumbnailContainerStyle, imageStyle } = styles;
 
   return(
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle} >
           <Image style={thumbnailStyle} source={{uri: thumbnail_image }} />
         </View>
         <View style={headerContentStyle} >
-          <Text>{title}</Text>
+          <Text style={headerTextStyle} >{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
+
+      <CardSection>
+        <Image style={imageStyle} source={ {uri: image } } />
+      </CardSection>
+
     </Card>
   );
 };
@@ -28,13 +33,29 @@ const styles = {
     justifyContent: 'space-around'
   },
 
+  headerTextStyle: {
+   fontSize: 18,
+    fontWeight: 'bold'
+  },
+
   thumbnailStyle : {
+    borderRadius: 25,
     height: 50,
     width: 50,
-    marginRight: 5
+  },
+
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
+  },
+
+  imageStyle: {
+    height: 300,
+    flex: 1,
+    width: null
   }
-
-
 
 };
 
