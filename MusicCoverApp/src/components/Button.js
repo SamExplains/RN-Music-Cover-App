1527e353ba853deddc8c51b50cf595749db57e9c
Sampuccino <Sampuccino@gmail.com>
2018-06-11 21:29:19 -0700

    Added Button and styles for that button.

--- MusicCoverApp/src/components/Button.js
+++ MusicCoverApp/src/components/Button.js
@@ -2,11 +2,35 @@ import React from 'react';
 import {Text, TouchableOpacity } from 'react-native';
 
 const Button = () => {
+  const {buttonStyle, textStyle} =  styles;
   return (
-    <TouchableOpacity>
-      <Text>Click Me</Text>
+    <TouchableOpacity style={buttonStyle}>
+      <Text style={textStyle}>Buy Now</Text>
     </TouchableOpacity>
   );
 };
+
+const styles = {
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#A55FC8',
+    borderBottomWidth: 2,
+    borderColor: '#000',
+    marginLeft: 5,
+    marginRight: 5
+  },
+
+  textStyle: {
+    alignSelf: 'center',
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  }
+
+};
+
 
 export default Button;
