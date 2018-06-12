e527528e7c73f10fd36dc9f527609aeb2171e07b
Sampuccino <Sampuccino@gmail.com>
2018-06-12 14:21:30 -0700

    Finished a faily standard and simple app.

--- MusicCoverApp/src/components/AlbumDetail.js
+++ MusicCoverApp/src/components/AlbumDetail.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import {Text, View, Image} from 'react-native';
+import {Text, View, Image, Linking} from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
 import Button from './Button';
 
 const AlbumDetail = ( { album } ) => {
-  const {title, artist, thumbnail_image, image } = album;
+  const {title, artist, thumbnail_image, image, url } = album;
   const {thumbnailStyle, headerContentStyle, headerTextStyle, thumbnailContainerStyle, imageStyle } = styles;
 
   return(
@@ -25,7 +25,10 @@ const AlbumDetail = ( { album } ) => {
       </CardSection>
 
       <CardSection>
-        <Button />
+        <Button onPress={() => Linking.openURL(url)}>
+          Purchase
+        </Button>
+
       </CardSection>
 
     </Card>
